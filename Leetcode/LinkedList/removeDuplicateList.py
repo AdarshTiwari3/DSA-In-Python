56de7ef5f0678f658e908ff6da3d10f030f17c6d
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2024-08-10 20:22:40 +0530

    remove-duplicates

--- Leetcode/LinkedList/removeDuplicateList.py
+++ Leetcode/LinkedList/removeDuplicateList.py
@@ -17,3 +17,25 @@ class Solution:
         
         return ans
         
+# more optimized code
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next: 
+            return head
+        curr=head
+        while curr.next:
+            
+            if curr.val==curr.next.val :
+                curr.next=curr.next.next
+
+            else:
+                curr=curr.next
+        
+        return head
+        
