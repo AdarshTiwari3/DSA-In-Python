c35c7453180210d3b438fc7f082e8edcb012191c
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2024-08-11 23:32:33 +0530

    swap-nodes-pair

--- Leetcode/LinkedList/swapNodes.py
+++ Leetcode/LinkedList/swapNodes.py
@@ -20,3 +20,25 @@ class Solution:
         swap1.val, swap2.val=swap2.val, swap1.val
 
         return head
+    
+
+#using recursion
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapRecursion(self,head):
+        if head is None or head.next is None:
+            return head
+        firstNode=head
+        secondNode=head.next
+        nextPairNode=secondNode.next
+        firstNode.next=self.swapRecursion(nextPairNode) # put the link of the next pair in the first.next
+        secondNode.next=firstNode
+        # now after swapping the second node will be the head
+        return secondNode
+
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return self.swapRecursion(head)
