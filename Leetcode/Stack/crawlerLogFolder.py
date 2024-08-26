18482cb8f43adc3d8d5b400d57e33bfa21c0d67d
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2024-08-27 00:33:03 +0530

    optimized-code-crawler-folder

--- Leetcode/Stack/crawlerLogFolder.py
+++ Leetcode/Stack/crawlerLogFolder.py
@@ -1,18 +1,19 @@
 from typing import List
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
-        stack=[]
-        # stack.append(logs[0])
+        cnt=0
         for i in range(len(logs)):
-            if stack and logs[i]=="../":
-                stack.pop()
-            elif logs[i]=="./":
+            if logs[i]=="../":
+                cnt-=1
+                if cnt<0:
+                    cnt=0
+            elif logs[i]=='./':
                 continue
-            elif logs[i]!="../":
-                stack.append(logs[i])
-                 
-        print("stack=",stack)
-        return len(stack)
+            elif logs[i]!='../':
+                cnt+=1
+
+        return cnt
+        
         
 sol=Solution()
 print(sol.minOperations(["d1/","d2/","../","d21/","./"])) #2
