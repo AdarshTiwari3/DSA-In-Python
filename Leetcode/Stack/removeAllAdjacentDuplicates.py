0bc2c1e0aa5d5ca9a953e8dea76f946c68e1e937
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2024-08-27 00:05:38 +0530

    remove-all-adjacent-duplicates

--- Leetcode/Stack/removeAllAdjacentDuplicates.py
+++ Leetcode/Stack/removeAllAdjacentDuplicates.py
@@ -3,15 +3,14 @@ class Solution:
         stack=[]
         stack.append(s[0])
         for i in range(1,len(s)):
-            j=0
+        
             if stack and stack[-1]==s[i]:
-                j=stack.pop()
-
-            
-            if not j:
+                stack.pop()
+                          
+            else:
                 stack.append(s[i])
         
         return "".join(stack)
-
+    
 sol=Solution()
 print(sol.removeDuplicates("abbaca")) #ca
