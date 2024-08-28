634a6ef64869d0cc5263f498cfcbf354852d7f76
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2024-08-29 00:02:15 +0530

    min-add-to-make-valid-parentheses

--- Leetcode/Stack/minAddToMakeValidParentheses.py
+++ Leetcode/Stack/minAddToMakeValidParentheses.py
@@ -10,3 +10,21 @@ class Solution:
     
 sol=Solution()
 print(sol.minAddToMakeValid("())")) #1
+
+
+#optimized solution
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        left_unmatched = 0  # Tracks unmatched '('
+        right_unmatched = 0  # Tracks unmatched ')'
+
+        for char in s:
+            if char == '(':
+                left_unmatched += 1
+            elif char == ')':
+                if left_unmatched > 0:
+                    left_unmatched -= 1  # Match a ')' with a '('
+                else:
+                    right_unmatched += 1  # Unmatched ')'
+        
+        return left_unmatched + right_unmatched
