4a527f7681b25b1d327e760c8bbdad44b0ab6eef
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2024-12-02 22:28:35 +0530

    Binary Tree Implementation

--- DSA/Tree/BinaryTree.py
+++ DSA/Tree/BinaryTree.py
@@ -11,8 +11,38 @@ class BinaryTreeImp:
     def __init__(self):
         self.root=None
 
-    def insertNode(self, val):
+    def insertANode(self, val):
         if self.root is None:
             self.root=TreeNode(val=val)
         else:
             self.insertNode(self.root,val)
+
+    def insertNode(self, curr, val):
+        queue=[curr]
+        #level order technique
+        while queue:
+            node=queue.pop(0)
+            if node.left is None:
+                node.left=TreeNode(val=val)
+                return
+            else:
+                queue.append(node.left)
+
+            if node.right is None:
+                node.right=TreeNode(val=val)
+                return
+            else:
+                queue.append(node.right)
+
+    def traversal_inorder(self,node):
+        if node:
+            self.traversal_inorder(node.left)
+            print(node.val,end='->')
+            self.traversal_inorder(node.right)
+
+
+tree=BinaryTreeImp()
+tree.insertANode(10)
+tree.insertANode(5)
+tree.insertANode(15)
+tree.traversal_inorder(tree.root)
