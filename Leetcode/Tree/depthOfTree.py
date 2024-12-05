65076ec7bbc2445d605066293e5ab0f04fe342f8
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2024-12-05 22:50:51 +0530

    depth of tree using BFS

--- Leetcode/Tree/depthOfTree.py
+++ Leetcode/Tree/depthOfTree.py
@@ -10,3 +10,32 @@ class Solution:
         if root is None:
             return 0
         return 1+max(self.maxDepth(root.left),self.maxDepth(root.right))
+    
+
+
+#iterative solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        depth=0
+        queue=[root]
+        while queue:
+            depth+=1
+
+            for i in range(len(queue)):
+                node=queue.pop(0)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+        return depth
