be847e5aaad65eb6806395917136c1f05cec6dc0
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2025-01-26 17:14:54 +0530

    power of 2

--- Leetcode/Recursion and Backtracking/Recursion/powerOfTwo.py
+++ Leetcode/Recursion and Backtracking/Recursion/powerOfTwo.py
@@ -7,3 +7,8 @@ class Solution:
         if n%2==0:
             return self.isPowerOfTwo(n//2)
         return False
+
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n>=1 and log2(n)%1==0 # using the log property
