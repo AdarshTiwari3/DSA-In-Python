a75982a91212274fd3c181f024ec80ac98248c01
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2025-01-26 17:28:47 +0530

    power calculation

--- Leetcode/Recursion and Backtracking/Recursion/powerOfFour.py
+++ Leetcode/Recursion and Backtracking/Recursion/powerOfFour.py
@@ -7,3 +7,16 @@ class Solution:
         if n%4==0:
             return self.isPowerOfFour(n//4)
         return False
+    
+
+#optimized solution
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n<=0:
+            return False
+        if n==1:
+            return True
+        logOfNumBase4=math.log(n)/math.log(4)
+        # print("logBase4=",logOfNumBase4)
+        return logOfNumBase4%1==0
+
--- Leetcode/Recursion and Backtracking/Recursion/powerOfThree.py
+++ Leetcode/Recursion and Backtracking/Recursion/powerOfThree.py
@@ -7,3 +7,11 @@ class Solution:
         if n%3==0:
             return self.isPowerOfThree(n//3)
         return False
+    
+#optimized solution
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        logValue = log10(n) / log10(3)
+        return logValue % 1==0
