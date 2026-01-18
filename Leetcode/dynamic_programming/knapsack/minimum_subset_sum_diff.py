7611592082feca045e295cf809cbc0af5d3cb1f3
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2026-01-18 10:53:44 +0530

    add solution of minimum subset sum difference

---
+++ Leetcode/dynamic_programming/knapsack/minimum_subset_sum_diff.py
@@ -0,0 +1,35 @@
+from typing import List
+import math
+
+class Solution:
+    def minimumDifference(self, nums: List[int]) -> int:
+        total_sum=sum(nums)
+
+        n=len(nums)
+
+        sum1=total_sum//2
+
+        dp=[False]*(sum1+1) #take the first half
+
+        dp[0]=True # base case
+
+        #. calculate the possible sum from 1 to total_sum range --> 1D dp of subset sum
+        
+        for i in range(n):
+            
+            for j in range(sum1, nums[i]-1, -1):
+                dp[j] = dp[j] or dp[j-nums[i]]
+
+
+        minn=math.inf
+        
+        for i in range(sum1,-1,-1):
+            if dp[i]:
+                minn=min(minn, total_sum-(2*i))
+                break
+
+        return minn
+
+sol=Solution()
+ans=sol.minimumDifference([1,6,5,11])
+print("ans=",ans) # 12-11 = 1
