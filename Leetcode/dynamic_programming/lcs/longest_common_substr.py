a09eef4e5d2d5c8caa289a87341bd7670c394fcf
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2026-02-06 10:29:47 +0530

    add full solution of longest common substring

--- Leetcode/dynamic_programming/lcs/longest_common_substr.py
+++ Leetcode/dynamic_programming/lcs/longest_common_substr.py
@@ -1,4 +1,5 @@
-class Solution:
+
+class SolutionRec:
     def longCommSubstr(self, s1, s2):
         # code here
         
@@ -23,9 +24,15 @@ class Solution:
         
         
         return helper(n , m , 0)
+
+sol_rec=SolutionRec()
+s1="ABCDGH"
+s2="ACDGHR"
+ans_rec=sol_rec.longCommSubstr(s1,s2)
+print("ans_rec=",ans_rec) #4
     
 # or 
-class Solution:
+class SolutionRec2:
     def longCommSubstr(self, s1, s2):
         # code here
         
@@ -59,9 +66,16 @@ class Solution:
         
         helper(n , m)
         return self.count
-        
+
+sol_rec2=SolutionRec2()
+s1="ABCDGH"
+s2="ACDGHR"
+ans_rec2=sol_rec2.longCommSubstr(s1,s2)
+print("ans_rec2=",ans_rec2) #4
+
+# TC=> O(2^(n+m)) SC=> O(n+m)
     
-class Solution:
+class SolutionMemo:
     def longCommSubstr(self, s1, s2):
         # code here
         
@@ -101,7 +115,50 @@ class Solution:
         
              
         
+sol_memo=SolutionMemo()
+s1="ABCDGH"
+s2="ACDGHR"
+ans_memo=sol_memo.longCommSubstr(s1,s2)
+print("ans_memo=",ans_memo) #4
+
+# TC=> O(n x m) SC=> O(n x m) + O(n + m) 
+
+# Tabulation solution
+
+
+class SolutionTab:
+    def longCommSubstr(self, s1, s2):
+        # code here
         
+        # tabulation solution
+        n=len(s1)
+        m=len(s2)
+        
+        dp=[[0 for _ in range(m+1)] for _ in range(n+1)]
+        
+        ans=0 # or ans = -math.inf
+        
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if s1[i-1] == s2[j-1]:
+                    dp[i][j]=1+dp[i-1][j-1]
+                else:
+                    dp[i][j]=0
+                ans=max(ans,dp[i][j])
+        
+        return ans
+                    
+    
+sol_tab=SolutionTab()
+s1="ABCDGH"
+s2="ACDGHR"
+ans_tab=sol_tab.longCommSubstr(s1,s2)
+print("ans_tab=",ans_tab) #4
+
+# Time: O(n × m)
+
+# Space: O(n × m)
+ 
         
 
 
