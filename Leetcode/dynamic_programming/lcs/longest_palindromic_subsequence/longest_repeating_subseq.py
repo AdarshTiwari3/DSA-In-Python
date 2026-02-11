47beb1c0b535f53e826f21af0abd3079d54851d9
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2026-02-11 21:04:43 +0530

    add solution of LRS( longest repeating subsequence) problem

---
+++ Leetcode/dynamic_programming/lcs/longest_palindromic_subsequence/longest_repeating_subseq.py
@@ -0,0 +1,24 @@
+# User function Template for python3
+
+
+class Solution:
+    def LongestRepeatingSubsequence(self, s):
+        # Code here
+
+        n = len(s)
+
+        # find the LCS
+
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                if s[i - 1] == s[j - 1] and i != j:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        return dp[n][n]
+
+
+# TC=> O(n x n) SC=>O(n x n)
