32380e04a7e8c48bcec088bfe68df503bc6196fc
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2026-02-16 12:32:15 +0530

    add memoized solution of mcm problem

--- Leetcode/dynamic_programming/mcm/mcm.py
+++ Leetcode/dynamic_programming/mcm/mcm.py
@@ -33,3 +33,43 @@ ans = sol.matrixMultiplication(arr)
 print("ans_rec=", ans)  # 4500
 
 # TC=> O(2^n) SC=> O(n)
+
+
+# Memoized Solution
+
+
+class SolutionMemo:
+    def matrixMultiplication(self, arr):
+        n = len(arr)
+        memo = [[-1] * (n + 1) for _ in range(n + 1)]
+
+        def helper(i, j):
+            if i >= j:
+                return 0
+
+            if memo[i][j] != -1:
+                return memo[i][j]
+
+            min_cost = math.inf
+
+            for k in range(i, j):
+                total_cost = (
+                    helper(i, k) + helper(k + 1, j) + arr[i - 1] * arr[k] * arr[j]
+                )
+
+                min_cost = min(min_cost, total_cost)
+            memo[i][j] = min_cost
+
+            return min_cost
+
+        return helper(1, n - 1)
+
+
+sol = SolutionMemo()
+arr = [10, 30, 5, 60]
+ans = sol.matrixMultiplication(arr)
+
+print("ans_rec=", ans)  # 4500
+
+# TC => O(n x n x n) # extra n is for loop
+# SC => O(n x n) + O(n) , n x n for memo table and extra n for recursion
