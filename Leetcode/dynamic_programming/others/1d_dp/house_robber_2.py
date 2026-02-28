514abc698ad4f028f83f16c62c24a97f0fbb4676
AdarshTiwari3 <a.tiwariadarsh99@gmail.com>
2026-02-28 12:29:22 +0530

    add most optimized solution of house robber 2

--- Leetcode/dynamic_programming/others/1d_dp/house_robber_2.py
+++ Leetcode/dynamic_programming/others/1d_dp/house_robber_2.py
@@ -62,3 +62,68 @@ class SolutionMemo:
     
 # TC=> O(n)
 # SC=> O(n x 2) + O(n) # recursion + memo map so total SC we can say will be O(n)
+
+
+
+class SolutionDP:
+    def rob(self, nums: List[int]) -> int:
+        n=len(nums)
+        
+        if n==1:
+            return nums[0]
+
+        def helper(start, last):
+            new_len=last-start+1
+
+            dp=[0] * (new_len+1)
+            dp[0]=0
+            dp[1]=nums[start]
+
+            for i in range(2, new_len+1):
+                dp[i]=max(nums[start + i-1]+dp[i-2], dp[i-1])
+
+            return dp[new_len]
+
+
+        #case 1 if we exclude the first element and case 2 if we exclude the last element
+
+        case1=helper(1, n-1)
+        case2=helper(0, n-2)
+        return max(case1, case2)
+    
+# TC=> O(n)
+# SC=> O(n) for dp array
+
+
+
+class SolutionMostOptimize:
+    def rob(self, nums: List[int]) -> int:
+        n=len(nums)
+        
+        if n==1:
+            return nums[0]
+
+        def helper(start, last):
+            new_len=last-start+1
+
+            
+            prev2=0
+            prev1=nums[start]
+
+            for i in range(2, new_len+1):
+                curr=max(nums[start + i - 1]+ prev2, prev1)
+                prev2=prev1
+                prev1=curr
+
+            return prev1
+
+
+        #case 1 if we exclude the first element and case 2 if we exclude the last element
+
+        case1=helper(1, n-1)
+        case2=helper(0, n-2)
+        return max(case1, case2)
+    
+# TC=> O(n)
+# SC=> O(1) 
+
